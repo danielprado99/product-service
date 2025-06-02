d0595f533ebaf0a9b471d47ad961aa6a80595358
Daniel Prado <daniel.vascomadagama2@gmail.com>
2025-06-02 18:47:08 -0300

    product-service

---
+++ src/main/java/store/product/Product.java
@@ -0,0 +1,17 @@
+package store.product;
+
+import java.util.Date;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Builder
+@Data @Accessors(fluent = true)
+public class Product {
+
+    private String id;
+    private String name;
+    private Float price;
+    private String unit;
+}
